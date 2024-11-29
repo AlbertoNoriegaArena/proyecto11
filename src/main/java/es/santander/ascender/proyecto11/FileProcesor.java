f979201e41d34740091869676d7c297bc6eb2ebb
curso06 <curso06@gmail.com>
2024-11-29 14:04:24 +0100

    faltan cosas de las vocales

--- src/main/java/es/santander/ascender/proyecto11/FileProcesor.java
+++ src/main/java/es/santander/ascender/proyecto11/FileProcesor.java
@@ -1,23 +1,39 @@
 package es.santander.ascender.proyecto11;
 
+import java.io.IOException;
+import java.lang.reflect.Array;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 public class FileProcesor implements IFileProcesor {
 
     @Override
-    public String leerFile(String filePath) throws Exception {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'leerFile'");
+
+    public String leerFile(String filePath) throws IOException {
+    
+        return Files.readString(Paths.get(filePath));
     }
 
     @Override
     public String eliminarVocales(String input) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'eliminarVocales'");
+
+        char [] vocales = {'a', 'e', 'i', 'o', 'u'};
+        for (int i = 0; i < input.length(); i++) {
+            for (int j = 0; j < vocales.length; j++) {
+                if (input.charAt(i) == vocales[j]) {
+                    
+                }
+            }
+        }
+
+        return;
     }
 
     @Override
     public void escribirAFile(String filePath, String content) throws Exception {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'escribirAFile'");
+        // Files.writeString => escribir el contenido en el archivo
+        Files.writeString(Path.of(filePath), content);
     }
-    
+
 }
